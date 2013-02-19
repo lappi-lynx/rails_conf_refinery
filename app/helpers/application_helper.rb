e9bdf82a4e862fa3e72906cf06ced9fe7df25cba
Stanislav Knyazev <knyazev@ziex.by>
2013-02-19 16:02:37 +0300

    Add base translation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def language_link
+    new_locale = ::Refinery::I18n.frontend_locales.detect{|locale| Globalize.locale.to_s != locale.to_s}
+    link_to t(new_locale), refinery.url_for(params.merge(locale: new_locale))
+  end
 end
