870e0324ecbd2f59ad3271ec91bf79a9496e4df0
Stanislav Knyazev <knyazev@ziex.by>
2013-02-19 17:04:40 +0300

    Add default template to speakers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def language_link
     new_locale = ::Refinery::I18n.frontend_locales.detect{|locale| Globalize.locale.to_s != locale.to_s}
-    link_to t(new_locale), refinery.url_for(params.merge(locale: new_locale))
+    link_to t(new_locale), refinery.url_for(params.merge(locale: new_locale)), :class => 'btn btn-danger'
   end
 end
