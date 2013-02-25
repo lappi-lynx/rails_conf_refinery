38f28ef27b0c2af4da4a08bee963ecacac92549a
Stanislav Knyazev <knyazev@ziex.by>
2013-02-25 14:21:06 +0300

    serialize social_contacts to Array trouble

--- vendor/extensions/speakers/app/models/refinery/speakers/speaker.rb
+++ vendor/extensions/speakers/app/models/refinery/speakers/speaker.rb
@@ -1,17 +1,15 @@
 module Refinery
   module Speakers
     class Speaker < Refinery::Core::BaseModel
-      # before_save :social_contacts_hash
-
-      serialize :social_contacts
+      serialize :social_contacts, Array
 
       # enumerate :social_contacts, :with => Refinery::SocialContact
 
       self.table_name = 'refinery_speakers'
 
-      attr_accessible :name, :country, :description, :social_contacts, :company, :photo_id, :position, :github, :twitter, :facebook, :google_plus
+      attr_accessible :name, :country, :description, :social_contacts, :company, :photo_id, :position, :github
 
-      attr_accessor :github, :twitter, :facebook, :google_plus
+      attr_accessor :github
 
       translates :name, :country, :description
 
@@ -23,13 +21,9 @@ module Refinery
 
       validates :name, :presence => true, :uniqueness => true
 
-      validates :github, :presence => true
+      # validates :github, :presence => true
 
       belongs_to :photo, :class_name => '::Refinery::Image'
-
-      # def social_contacts_hash
-      #   social_contacts = {"github" => self.github.split, "twitter" => self.twitter.split, "facebook" => self.facebook.split, "google_plus" => self.google_plus.split }
-      # end
     end
   end
 end
