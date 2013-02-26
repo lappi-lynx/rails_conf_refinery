e7b19551ae9d121bf1fe43242bcf0f89733a0801
Stanislav Knyazev <knyazev@ziex.by>
2013-02-26 15:31:57 +0300

    Show contacts in show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,14 @@ module ApplicationHelper
     new_locale = ::Refinery::I18n.frontend_locales.detect{|locale| Globalize.locale.to_s != locale.to_s}
     link_to t(new_locale), refinery.url_for(params.merge(locale: new_locale)), :class => 'btn btn-danger switch-lang'
   end
+
+  def show_contact(contacts)
+    contact_list = ''
+    contacts.each do |contact|
+      if contact[:value].present?
+        contact_list << "<div><b>#{contact[:name]}:</b> #{contact[:value]}</div>"
+      end
+    end
+    contact_list.html_safe
+  end
 end
